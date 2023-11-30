73772fae239180833c7bb47c4e9a10133c84935e
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-11-30 19:04:05 +0900

    [level unrated] Title: 모음 제거, Time: 0.10 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string.replaceAll("[aeiou]", "");
         // my_string에서 모음을 제거한 후 리턴
+        String answer = my_string.replaceAll("[aeiou]", "");
         
         return answer;
     }
