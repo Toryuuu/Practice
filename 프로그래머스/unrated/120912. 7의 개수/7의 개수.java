40bc4e2e3792e1363d340704fed9bc4c71ff8eda
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-12-18 18:55:04 +0900

    [level unrated] Title: 7의 개수, Time: 0.48 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/unrated/120912. 7의 개수/7의 개수.java
@@ -1,25 +1,17 @@
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        int count = 0;
-        String[] str = new String[array.length];
         
-        // 문자열 배열에 array를 담아줌
         for(int i = 0; i < array.length; i++) {
-          str[i] = Integer.toString(array[i]);
-        }
-        
-        for(int i = 0; i < str.length; i++) {
-            String[] split = str[i].split("");
+            String str = Integer.toString(array[i]);
+            String[] split = str.split("");
             
             for(int j = 0; j < split.length; j++) {
                 if(split[j].equals("7")) {
-                    count++;
+                    answer++;
                 }
             }
         }
-        
-        answer = count;
         
         return answer;
     }
