eb165b5a292b0a03beddaf0ebcf32fa7762ffbac
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-12-19 17:55:40 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.java
@@ -1,26 +1,24 @@
+import java.util.ArrayList;
+
 class Solution {
     public String[] solution(String my_str, int n) {
         
-        int len = 0; // answer의 크기(잘라낼 덩어리 갯수)
+        ArrayList<String> list = new ArrayList<>();
+        int len = my_str.length(); // 문자열 길이
         
-        if(my_str.length() % n == 0) {
-            len = my_str.length() / n;
-        } else if(my_str.length() % n > 0) {
-            len = (my_str.length() / n) + 1;
-        }
-       
-        String[] answer = new String[len];
+        // 문자열이 n으로 나눠떨어지는 경우
+        for(int i = 0; i < (len / n); i++) {
+            list.add(my_str.substring((n * i), (n * (i + 1)))); // end 인자에 +1 꼭 해주기
+}
         
-        // 각 덩어리를 대상으로 자르기
-        for(int i = 0; i < len; i++) {
-            if(my_str.length() >= n) {
-                // n씩 나눌 수 있는 경우
-                answer[i] = my_str.substring(0, n); // 0부터 n-1 인덱스까지 answer에 대입
-                my_str = my_str.substring(n, my_str.length()); // 문자열을 n부터 끝까지로 재설정
-            } else // 나머지 부분
-                answer[i] = my_str.substring(0, my_str.length()); // 나머지 부분 : 처음부터 끝까지
+        // 문자열이 n으로 나눠떨어지지 않는 경우 나머지 처리
+        if(len % n != 0) {
+            list.add(my_str.substring((len / n) * n));
         }
         
+        // list를 문자열 배열로 변환
+        String[] answer = list.toArray(new String[0]);
+            
         return answer;
     }
 }
