80ea5362fff6162db66e7ccc835cad16595eadba
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-12-19 18:18:51 +0900

    [level unrated] Title: 중복된 숫자 개수, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.java
+++ 프로그래머스/unrated/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -1,21 +1,9 @@
 class Solution {
     public int solution(int[] array, int n) {
         int answer = 0;
-        String s = Integer.toString(n);
         
-        // 문자열 배열 생성
-        String[] str = new String[array.length];
-        
-        // 문자열 배열에 array 대입
         for(int i = 0; i < array.length; i++) {
-            str[i] = Integer.toString(array[i]);
-        }
-        
-        // 개수 세기
-        for(int i = 0; i < str.length; i++) {
-            if(!str[i].equals(s)) {
-                continue;
-            } else
+            if(array[i] == n) 
                 answer++;
         }
         
