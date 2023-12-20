2ab51895bba8b8986a94ab5bef542be972c374f1
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-12-20 16:26:51 +0900

    [level 1] Title: 평균 구하기, Time: 2.31 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,11 +1,9 @@
+import java.util.stream.IntStream;
+
 class Solution {
     public double solution(int[] arr) {
-        double answer = 0;
         
-        for(int i = 0; i < arr.length; i++) {
-            answer += (double)arr[i]; 
-        }
-        
-        return answer/arr.length;
+        // stream으로 구하기
+        return IntStream.of(arr).average().orElse(0);
     }
 }
