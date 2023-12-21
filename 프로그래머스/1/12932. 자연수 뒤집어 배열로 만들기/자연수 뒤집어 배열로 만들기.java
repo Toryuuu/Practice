7d0d73d9a1f3fa7797970dd8c593289840661db6
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-12-21 16:17:25 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 6.32 ms, Memory: 82.6 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -2,15 +2,14 @@ class Solution {
     public int[] solution(long n) {
         
         String str = "" + n; // n 문자열로 변환
-        int len = str.length();
-        int[] answer = new int[len];
+        int[] answer = new int[str.length()];
+        int index = 0;
         
-        // answer의 역순인덱스부터 str 원소값 순차대입
-        // len -  1 하는 이유 : 인덱스는 0부터 시작하므로
-        for(int i = len - 1; i >= 0; i--) {
-            answer[i] = str.charAt(len - i - 1) - '0';
-            // len - i - 1으로 index 0부터 순차적으로 접근
-            // '0'(아스키코드 48)을 빼서 int로 변환 후 대입
+        while(n > 0) {
+            answer[index++] = (int)(n % 10); 
+            // 일의자리수부터 answer에 순차대입
+            n /= 10; 
+            // 일의자리수부터 하나씩 제거 후 반복
         }
         
         return answer;
