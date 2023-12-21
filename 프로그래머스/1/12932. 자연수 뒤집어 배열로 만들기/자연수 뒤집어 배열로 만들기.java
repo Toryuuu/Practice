e61729ac43aa5446f2c3b373d776b7a57faf2aa1
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-12-21 16:01:05 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 5.42 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -1,13 +1,16 @@
 class Solution {
     public int[] solution(long n) {
         
-        // 하나씩 분리하여 문자열 배열에 저장
-        String[] s = Long.toString(n).split("");
+        String str = "" + n; // n 문자열로 변환
+        int len = str.length();
+        int[] answer = new int[len];
         
-        int[] answer = new int[s.length];
-        
-        for(int i = 0; i < s.length; i++) {
-            answer[i] = Integer.parseInt(s[s.length - 1 - i]); // s의 역순부터 대입
+        // answer의 역순인덱스부터 str 원소값 순차대입
+        // len -  1 하는 이유 : 인덱스는 0부터 시작하므로
+        for(int i = len - 1; i >= 0; i--) {
+            answer[i] = str.charAt(len - i - 1) - '0';
+            // len - i - 1으로 index 0부터 순차적으로 접근
+            // '0'(아스키코드 48)을 빼서 int로 변환 후 대입
         }
         
         return answer;
