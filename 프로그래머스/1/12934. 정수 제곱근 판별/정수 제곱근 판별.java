069e3712e32e7f81c3918a8ff81155a9f10f21ed
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-12-23 20:29:49 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -2,18 +2,10 @@ import java.lang.Math;
 
 class Solution {
     public long solution(long n) {
-        long answer = -1; // -1로 초기화
-        int x = 1; // 양의 정수의 최솟값 1로 초기화
         
-        // x 제곱이 n 이하일 동안 반복
-        while((long)Math.pow(x, 2) <= n) {
-            if((long)Math.pow(x, 2) == n) {
-                answer = (long)Math.pow((x + 1), 2);
-                break; // 값 찾으면 빠져나오기
-            }  
-            x++; // x값 증가시켜 반복
-        }
-         
-        return answer;
+        if(Math.pow((int)Math.sqrt(n), 2) == n) {
+            return (long)Math.pow(Math.sqrt(n) + 1, 2);
+        } else
+            return -1;
     }
 }
