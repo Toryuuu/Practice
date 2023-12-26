c26996c11da487d248afdbefc77293ff68bc2914
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2023-12-27 03:40:42 +0900

    [level 1] Title: 하샤드 수, Time: 5.41 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,20 +1,13 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = false;
         
-        int origin = x; // x의 원래 값 저장
-        int sum = 0; // 자릿수의 합
+        String s = "" + x; // x 문자열로 변환
+        int sum = 0; // 자릿수 합
         
-        while(x >= 1) {
-            sum += (x % 10); // 일의자리수 계속 더해줌
-            x /= 10; // x에서 일의자리수 제거 후 반복
+        for(int i = 0; i < s.length(); i++) {
+            sum += s.charAt(i) - '0'; // -'0' : 정수로 변환
         }
         
-        // x가 자릿수의 합으로 나눠떨어지면 true 리턴
-        if(origin % sum == 0) {
-            answer = true;
-        }
-        
-        return answer;
+        return (x % sum == 0)? true: false;
     }
 }
