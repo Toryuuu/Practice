081fc71d13c3d4fb1490c4357f3c93de20a3d7f8
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-03 22:23:05 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -1,15 +1,13 @@
 class Solution {
     public String solution(String phone_number) {
-        String answer = "";
-        String[] num = phone_number.split(""); // 문자열 분리 후 배열에 저장
         
-        for(int i = 0; i < num.length; i++) {
-            if(i < num.length - 4) {
-                answer += "*";
-            } else
-                answer += num[i];
+        // 문자배열로 변환
+        char[] num = phone_number.toCharArray();
+        
+        for(int i = 0; i < num.length - 4; i++) {
+           num[i] = '*'; // 대체
         }
-        
-        return answer;
+        // 문자열로 변환하여 리턴
+        return new String(num);
     }
 }
