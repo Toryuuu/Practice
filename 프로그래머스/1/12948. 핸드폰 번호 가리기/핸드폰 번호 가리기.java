cec90eff1fae1d4fb681637d3d1e8d0876e26776
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-03 22:20:21 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 2.07 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -2,19 +2,13 @@ class Solution {
     public String solution(String phone_number) {
         String answer = "";
         String[] num = phone_number.split(""); // 문자열 분리 후 배열에 저장
-        // 값을 담을 StringBuilder 생성
-        StringBuilder sb = new StringBuilder();
         
-        // 마지막에서 4번째 인덱스까지 *로 대체하고 StringBuilder에 추가
-        for(int i = 0; i < num.length - 4; i++) {
-            sb.append("*");
+        for(int i = 0; i < num.length; i++) {
+            if(i < num.length - 4) {
+                answer += "*";
+            } else
+                answer += num[i];
         }
-        // 마지막 4자리 숫자를 StringBuilder에 추가
-        for(int i = num.length - 4; i < num.length; i++) {
-            sb.append(num[i]);
-        }
-        // StringBuilder를 문자열로 변환 후 리턴
-        answer = sb.toString();
         
         return answer;
     }
