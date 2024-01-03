d93552b86764ca2a18e06dc9de5b7c1dccfb0703
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-03 21:23:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,19 +1,12 @@
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int[] num = new int[10]; // 크기가 10인 배열
         
-        // 주어진 배열의 원소값에 해당하는 num 인덱스에 1 대입
-        for(int i = 0; i < numbers.length; i++) {
-            num[numbers[i]] = 1; 
+        int sum = 45; // 0부터 9를 더한 값
+        
+        for(int i : numbers) {
+            sum -= i; // 합계에서 numbers에 있는 숫자를 빼줌
         }
         
-        // 원소값이 1이 아닌 인덱스를 모두 더해줌
-        for(int i = 0; i < num.length; i++) {
-            if(num[i] != 1)
-                answer += i;
-        }
-        
-        return answer;
+        return sum;
     }
 }
