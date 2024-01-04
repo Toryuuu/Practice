8d8a2ed5f38a6fc81a21d953bc39b4b5ecdcd2a8
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-04 17:52:19 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.07 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,28 +1,27 @@
 class Solution {
     public int[] solution(int[] arr) {
         
-        // 배열 길이가 1인 경우 -1만 담은 배열 리턴
-        if(arr.length == 1) {
-            return new int[]{-1}; 
-        }
+        int size = arr.length;
+        int[] answer = new int[size - 1];
         
-        int min = arr[0]; // 최솟값 임시지정
-        
-        // 배열 arr에서 가장 작은 수 찾기(주의 : i 1부터 시작)
-        for(int i = 1; i < arr.length; i++) {
-            if(arr[i] < min)
-                min = arr[i];
-        }
-        
-        // 가장 작은 수를 제외하고 배열에 담아주기
-        int[] answer = new int[arr.length - 1];
-        int index = 0;
-        
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] != min)
-                answer[index++] = arr[i];
-        }
-        
+        if(size == 1) {
+            arr[0] = -1;
+            return arr; // 배열크기가 1이면 -1 담아 리턴
+        } else {
+            int min = Integer.MAX_VALUE; // 최솟값. 정수의 최댓값 임시지정
+            // 최솟값 가려내기
+            for(int i = 0; i < size; i++) {
+                if(arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            // 최솟값을 제외하고 배열에 담은 후 리턴
+            int index = 0;
+            for(int i = 0; i < size; i++) {
+                if(arr[i] != min)
+                    answer[index++] = arr[i];
+            }
+        }      
         
         return answer;
     }
