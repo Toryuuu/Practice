175d6f0228235c9040c0206c566523e9232bae15
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-08 16:25:41 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -2,11 +2,16 @@ class Solution {
     public boolean solution(String s) {
         boolean answer = true;
         
-        // 4개 혹은 6개의 숫자로 구성된 문자열
-        String str = "\\d{4}|\\d{6}"; 
-        
-        if(!s.matches(str))
-            answer = false;
+        if(s.length() == 4 || s.length() == 6) {
+            try {
+                int x = Integer.parseInt(s); // 정수형으로 변환 
+                answer = true; // 변환 가능할 경우 true
+            } catch(NumberFormatException e) {
+                answer = false; // 변환 불가능할 경우 false 리턴
+            }
+            
+        } else
+            answer = false; // 길이가 4 혹은 6이 아니면 false 리턴
         
         return answer;
     }
