4c32459e51484a339d3390d1577ef1a39bf8e931
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-12 20:32:38 +0900

    [level 1] Title: 예산, Time: 0.50 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -11,9 +11,10 @@ class Solution {
             // 신청금액이 예산 이하인 경우
             if(sum <= budget) {
                 answer = d.length; // 모든 부서 지원 가능
-            } // 신청금액이 예산을 초과하는 경우 
+            } 
+            // 신청금액이 예산을 초과하는 경우 
             else {
-                answer = i;
+                answer = i; // 초과하기 직전 인덱스개의 부서 지원 가능
                 break;
             }
         }
