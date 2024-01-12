2346c97603b6c6e28ae88c24e8088985bcbc55af
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-12 20:06:16 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.14 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -5,15 +5,11 @@ class Solution {
         // n을 10진수 -> 3진수로 변환
         String three = Integer.toString(n, 3);
         
-        // 3진수를 한자리씩 분리 후 배열에 담기
-        String[] str = three.split("");
+        // 3진수를 담은 StringBuffer 생성
+        StringBuffer sb = new StringBuffer(three);
         
-        String reverse = ""; // 뒤집은 숫자를 담을 문자열
-        
-        // 3진수를 역순으로 문자열에 담아주기
-        for(int i = 0; i < str.length; i++) {
-            reverse += str[str.length - 1 - i];
-        }
+        // sb의 원소를 역순으로 담는 문자열
+        String reverse = sb.reverse().toString();
         
         // 뒤집힌 3진수 -> 10진수로 변환
         answer = Integer.parseInt(reverse, 3);
