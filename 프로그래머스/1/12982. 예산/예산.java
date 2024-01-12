103263b2d45e9e01418ed4e7017df3e1870001f1
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-12 20:44:28 +0900

    [level 1] Title: 예산, Time: 0.46 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -3,19 +3,14 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] d, int budget) {
         int answer = 0;
-        int sum = 0; // 신청금액의 합
+        
         Arrays.sort(d); // 오름차순 배열
         
-        for(int i = 0; i < d.length; i++) {
-            sum += d[i];
-            // 신청금액이 예산 이하인 경우
-            if(sum <= budget) {
-                answer = d.length; // 모든 부서 지원 가능
-            } 
-            // 신청금액이 예산을 초과하는 경우 
-            else {
-                answer = i; // 초과하기 직전 인덱스개의 부서 지원 가능
-                break;
+        for(int i : d) {
+            // 각 부서의 지원금이 예산 이하면
+            if(i <= budget) {
+                answer++; // 부서 개수 1씩 추가
+                budget -= i; // 예산에서 해당 금액 빼기
             }
         }
         
