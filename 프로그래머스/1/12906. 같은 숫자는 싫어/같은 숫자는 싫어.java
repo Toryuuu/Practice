ad686dc9cd598f145bb1c7d732d93fbffbd71cc0
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-12 19:11:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 26.56 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,20 +3,23 @@ import java.util.*;
 public class Solution {
     public int[] solution(int []arr) {
         
-        // 배열 arr을 담은 ArrayList 생성
         ArrayList<Integer> list = new ArrayList<>();
         
+        // 배열 arr의 연속되지 않는 원소만 list에 담기
         for(int i = 0; i < arr.length; i++) {
+            // 마지막 원소가 아닐 경우
             if(i != arr.length - 1) {
                 if(arr[i] != arr[i + 1]) {
-                    list.add(arr[i]);
+                    list.add(arr[i]); // 다음 원소와 같지 않으면 list에 추가
                 } 
-            } else
+            // 마지막 원소는 무조건 list에 추가
+            } else 
                 list.add(arr[arr.length - 1]);
         }
-        
+        // list와 같은 크기의 배열 생성
         int[] answer = new int[list.size()];
         
+        // answer에 list의 원소 옮겨담기
         for(int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
         }
