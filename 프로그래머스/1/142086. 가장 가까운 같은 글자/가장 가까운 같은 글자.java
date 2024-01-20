77da638840a96734208830296ad8eb797180f1f5
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-20 22:02:17 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 20.08 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
+        answer[0] = -1; // 0번째 인덱스는 -1
         
         for(int i = 0; i < s.length(); i++) {
-            if(i != 0) {
-                int x = s.substring(0, i).lastIndexOf(s.charAt(i));
-                // 똑같은 값이 존재할 경우
-                if(x != -1) {
-                    answer[i] = i - x; 
-                } else {
-                    answer[i] = x; // 똑같은 값 없으면 -1
-                }
-            } else
-                answer[i] = -1; // 0번째 인덱스는 -1
+            // 0 ~ i-1 인덱스값 중에서 i인덱스값이 마지막으로 나온 인덱스 찾기
+            int x = s.substring(0, i).lastIndexOf(s.charAt(i));
+            // 똑같은 값이 존재할 경우
+            if(x != -1) {
+                answer[i] = i - x; 
+            } else {
+                answer[i] = x; // 똑같은 값 없으면 -1
+            }
         }
             
         return answer;
