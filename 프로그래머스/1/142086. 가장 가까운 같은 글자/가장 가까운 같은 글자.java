f201854563bed85ebabf7dc16f27276749fac64c
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-01-20 22:11:45 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 18.96 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[] solution(String s) {
-        int[] answer = new int[s.length()];
-        answer[0] = -1; // 0번째 인덱스는 -1
         
-        for(int i = 0; i < s.length(); i++) {
-            // 0 ~ i-1 인덱스값 중에서 i인덱스값이 마지막으로 나온 인덱스 찾기
-            int x = s.substring(0, i).lastIndexOf(s.charAt(i));
-            // 똑같은 값이 존재할 경우
-            if(x != -1) {
-                answer[i] = i - x; 
-            } else {
-                answer[i] = x; // 똑같은 값 없으면 -1
+        String[] arr = s.split(""); // 하나씩 떼서 배열에 저장
+        int[] answer = new int[arr.length];
+        answer[0] = -1; // 0번째 인덱스는 -1
+   
+        // 이중for문으로 같은 값 찾기
+        for(int i = 1; i < arr.length; i++) {
+            answer[i] = -1; // 디폴트는 -1
+            // 0부터 i-1 인덱스까지 역순으로 검색 
+            for(int j = i - 1; j >= 0; j--) {
+                if(arr[j].equals(arr[i])) {
+                    answer[i] = i - j;
+                    break;
+                }
             }
         }
             
