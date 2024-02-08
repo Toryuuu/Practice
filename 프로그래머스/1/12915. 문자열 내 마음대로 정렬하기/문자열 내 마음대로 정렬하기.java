0307da729ac0506455905da7ecfaaa8969f43cdf
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-02-08 21:22:41 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 2.32 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,22 +1,11 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public String[] solution(String[] strings, int n) {
-        String[] answer = new String[strings.length];
-        int cnt = 0;
         
         Arrays.sort(strings); // 오름차순 정렬
+        Arrays.sort(strings, Comparator.comparing(s -> s.substring(n, n+1))); // 각 문자열의 n번째 문자를 기준으로 정렬
         
-        // a ~ z 아스키코드 : 97 ~ 122
-        for(int i = 97; i < 123; i++) {
-            for(int j = 0; j < strings.length; j++) {
-                // 각 단어의 인덱스 n의 문자가 아스키코드와 일치하면
-                if(strings[j].charAt(n) == i) {
-                    answer[cnt++] = strings[j]; // answer에 처음부터 차례로 넣어줌
-                }
-            }
-        }
-        
-        return answer;
+        return strings;
     }
 }
