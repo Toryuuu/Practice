c94f37195aea907d94022429c83b2c71c5f89f7e
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-02-08 21:42:32 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 10.17 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -3,9 +3,22 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] strings, int n) {
         
-        Arrays.sort(strings); // 오름차순 정렬
-        Arrays.sort(strings, Comparator.comparing(s -> s.substring(n, n+1))); // 각 문자열의 n번째 문자를 기준으로 정렬
+        String[] answer = new String[strings.length];
+        List<String> list = new ArrayList<>();
         
-        return strings;
+        // 각 문자열의 인덱스 n번째 글자 + 배열 strings 요소들을 list에 담아주기 
+        for(int i = 0; i < strings.length; i++) {
+            list.add(strings[i].charAt(n) + strings[i]); 
+        }
+        
+        // n번째 글자 순으로 오름차순 정렬
+        Collections.sort(list); 
+        
+        // list 요소의 0번 인덱스 제외하고 answer에 담기 
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i).substring(1);
+        }
+        
+        return answer;
     }
 }
