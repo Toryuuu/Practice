18f38eb4da402be7eee6147c7e353cc086604fca
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-02-11 19:52:22 +0900

    [level 1] Title: K번째수, Time: 0.35 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -6,13 +6,15 @@ class Solution {
         
         for(int i = 0; i < commands.length; i++) {
             int[] c = commands[i];
+            int[] temp = new int[c[1] - (c[0] - 1)];
             
-            // 배열 array 자르고 정렬하기
-            int[] cut = Arrays.copyOfRange(array, c[0] - 1, c[1]); // n번째 숫자를 구해야 하므로 인덱스-1부터 시작, 메서드에서 3번째 인자는 결과에 미포함이므로 -1 안 해줌.
-            Arrays.sort(cut); // 오름차순 정렬
-            
+            for(int j = 0; j < temp.length; j++) {
+               temp[j] = array[j + (c[0] - 1)];
+                // c[0] - 1 인덱스부터 1씩 증가시킨 원소 저장
+            }
+            Arrays.sort(temp); // 오름차순 정렬
             // K번째 수를 뽑아내서 answer에 대입
-            answer[i] = cut[c[2] - 1]; // "번째" 수 -> -1 해줌
+            answer[i] = temp[c[2] - 1]; // "번째" 수 -> -1 해줌
         }
         
         return answer;
