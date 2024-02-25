f7869d6125f0cfe4601c1d631af17153a6f5fcbb
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-02-25 18:09:17 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 36.97 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,36 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        // 공백 기준으로 나누어 배열에 저장
+        String[] words = s.split(" ", -1);
+        
+        for(int i = 0; i < words.length; i++) {
+            String word = words[i];
+            
+            // 공백문자가 연속해서 나올 경우 답에도 공백 더해줌
+            if(word.isEmpty()) {
+                answer += " ";
+                continue;
+            }
+            
+            // word의 0번 인덱스(첫글자)
+            char init = word.charAt(0);
+            
+            // 모두 소문자로 변환 후 첫글자만 대문자로 변환
+            word = word.toLowerCase();
+            if(Character.isDigit(init)){
+                answer += word + " ";
+            } else {
+                word = Character.toUpperCase(init) + word.substring(1);
+                answer += word + " ";
+            }
+        }
+            // 마지막에 있는 공백 제거
+            if(answer.length() > 0 && answer.charAt(answer.length() - 1) == ' ') {
+                answer = answer.substring(0, answer.length() - 1);
+            }
+             
+        return answer;
+    }
+}
