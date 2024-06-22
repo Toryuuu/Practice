277f7ba169e99076e20f664f5cc22f1c3f5a6999
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-06-22 21:09:48 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.50 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,23 +1,18 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int temp = 0; // 음수 중 가장 큰 두 값을 곱한 값
-        int temp2 = 0; // 양수 중 가장 큰 두 값을 곱한 값
         
         // 오름차순 정렬
         Arrays.sort(numbers);
         
-        // 가장 작은 수가 음수일 때 -> 바로 오른쪽에 있는 음수와 곱하기
-        if(numbers[0] < 0 && numbers[1] < 0) {
-            temp = numbers[0] * numbers[1];
-            temp2 = numbers[numbers.length - 1] * numbers[numbers.length - 2]; 
-            answer = (temp < temp2)? temp2: temp;
-        } else { // 가장 큰 수가 양수일 때 -> 바로 왼쪽에 있는 수와 곱하기
-            answer = numbers[numbers.length - 1] * numbers[numbers.length - 2]; 
-        }
+        // 가장 큰 수끼리의 곱
+        int max1 = numbers[numbers.length - 1] * numbers[numbers.length - 2];
         
-        return answer;
+        // 가장 작은 수끼리의 곱(둘 다 음수일 경우 양이 되므로)
+        int max2 = numbers[0] * numbers[1];
+        
+        // 둘 중 더 큰 값을 리턴
+        return Math.max(max1, max2);
     }
 }
