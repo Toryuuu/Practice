d94e0916f1ea77c54fe6a06bd7fd00251269b056
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-08-04 21:52:51 +0900

    [level 0] Title: 등수 매기기, Time: 0.87 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.java
@@ -1,38 +1,24 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[][] score) {
-        int len = score.length;
-        int[] answer = new int[len];
-        double[][] save = new double[len][2];
+        int[] answer = new int[score.length];
         
-        for(int i = 0; i < len; i++) {
-            double average = (score[i][0] + score[i][1]) / 2.0; // 각 학생의 영어, 수학 평균
-            
-            save[i][0] = average; // 평균값 저장
-            save[i][1] = i; // 식별을 위해 인덱스 저장
+        List<Integer> list = new ArrayList<>();
+        
+        // 배열 단위로 이차원 배열 score 순회 
+        for(int[] i : score) {
+            list.add(i[0] + i[1]); // 점수의 합을 list에 삽입
         }
         
-        // 람다식 이용한 내림차순 정렬(평균값 기준)
-        Arrays.sort(save, (a, b) -> Double.compare(b[0], a[0]));
+        // 합계순 내림차순 정렬
+        list.sort(Comparator.reverseOrder());
         
         // 등수 매기기
-        int rank = 1; // 1등에서 시작 -> 1씩 커지므로
-        
-        for(int i = 0; i < len;) {
-            int same = 1; // 점수가 같은 사람 수
-            answer[(int)save[i][1]] = rank; // 원래 인덱스에 등수 대입 
-            
-            // 같은 점수일 때 처리
-            while(i + same < len && save[i][0] == save[i + same][0]) {
-                answer[(int)save[i + same][1]] = rank;
-                same++; // 같은 등수인 사람 증가시키기
-            }
-            
-            i += same; // 같은 점수 -> 반복문 skip
-            rank += same; // 등수 뛰어넘기(like 유도 동메달)
+        for(int i = 0; i < score.length; i++) {
+            answer[i] = list.indexOf(score[i][0] + score[i][1]) + 1; // 등수는 1부터 시작이므로 1 더하기
         }
-              
+        
         return answer;
     }
 }
