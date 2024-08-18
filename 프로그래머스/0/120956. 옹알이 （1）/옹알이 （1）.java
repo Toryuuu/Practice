91bfb0f77009de8aaa3982d5fad6fc9f027e85d1
Toryuuu <134052627+Toryuuu@users.noreply.github.com>
2024-08-18 15:49:56 +0900

    [level 0] Title: 옹알이 (1), Time: 7.43 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -5,25 +5,14 @@ class Solution {
         String[] words = {"aya", "ye", "woo", "ma"};
         
         for(String word : babbling) {
-            int i = 0; // 문자열 word의 현재 탐색 위치
-            while(i < word.length()) {
-                boolean match = false;
-                for(String w : words) {
-                    if(i + w.length() <= word.length() && word.substring(i, i + w.length()).equals(w)) {
-                        i += w.length(); // i를 단어 길이만큼 증가 후 반복
-                        match = true;
-                        break;
-                    }
-                }
-                if(!match) {
-                    break; // false일 경우 빠져나감
-                }
+            for(String w : words) {
+                word = word.replaceFirst(w, "-");
             }
-            if(i == word.length()) {
-                answer++; // word가 발음할 수 있는 단어로 이루어져 있음  
+            if(word.replaceAll("-", "").isEmpty()) {
+                answer++;
             }
         }
-        
+            
         return answer;
     }
 }
